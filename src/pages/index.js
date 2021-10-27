b4813d05cd1f1dfb941770c5514938cd7556556d
Kieran C <kieranakc2@gmail.com>
2021-10-27 13:20:01 +0100

    supress gatsby warning css @import syntax

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,44 @@
 import * as React from 'react';
-import styled, { createGlobalStyle } from 'styled-components';
+import { Helmet } from 'react-helmet';
+import { createGlobalStyle } from 'styled-components';
+import NavBar from './../components/Navbar';
 
 const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Prompt:wght@100;400;900&display=swap');
-
   *, :after, :before {
     box-sizing: border-box;
   }
 
+  :root {
+    --navbar-color: #18181b;
+  }
+
   body {
+    /* font-size: 10px; */
+    color: white;
     font-size: 10px;
-    
     margin: 0;
     padding: 0;
     font-family: 'Prompt', sans-serif;
   }
 `;
 
-const HeyThere = styled.div`
-  color: red;
-`;
-
 const IndexPage = () => {
   return (
     <>
+      <Helmet>
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossorigin
+        ></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Prompt:wght@100;400;900&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Helmet>
       <GlobalStyle />
-      <HeyThere>Hello world!</HeyThere>
+      <NavBar />
     </>
   );
 };
