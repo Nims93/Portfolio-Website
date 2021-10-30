f8db4eb718c14a0fe3cd5f998c115b945264a9eb
Kieran C <kieranakc2@gmail.com>
2021-10-30 13:51:25 +0100

    updated desc of each project + portfolio desc

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,41 +2,42 @@ import React from 'react';
 import styled from 'styled-components';
 import Project from './Project';
 import WaveSvg from './../svgs-as-components/singlewave2.svg';
+import GitHubSvg from '../svgs-as-components/icons8-github.svg';
 import ReactMusicAppWebmDemo from './../videos/React-Music-Player.webm';
 import ReactRealtimeChatAppWebmDemo from './../videos/React-Chat-App.webm';
 import PathfindingVisualiserWebmDemo from './../videos/Pathfinding-Visualiser-Demo.webm';
 
 const PROJECTS_INFO = [
   {
-    title: 'React Music Player',
-    videoSrc: ReactMusicAppWebmDemo,
-    altText: 'Music player app demo webm',
-    desc: 'A music player app that plays a selection of songs using the javascript Audio contructor. Able to handle variable playback speeds without the app losing sync with the Audio APIs playback rate whether due to an increase playback rate from user seekbar scrubbing or a decrease from low download speeds. Also features a nice elegant and responsive sliding UI design. Coded with React and SASS.',
-    srcCodeLink: 'https://github.com/Nims93/react-music-player',
-    liveLink:
-      'https://615504cc4d11864b80b8bc91--gallant-ride-7ea3b5.netlify.app/',
+    title: 'Pathinding Visualiser',
+    videoSrc: PathfindingVisualiserWebmDemo,
+    altText: 'Pathfinding viualiser demo webm',
+    desc: 'An app that visualises pathfinding and maze generation algorithms. Create a maze of walls with mouse clicks, click draging your cursor across cells or selecting one of the maze generation algorithms. Then use a pathfinding algorithm to find a path (if one exists) from the start node to the end node. My first real project and the one that solidified my basic knowledge on data structures, thinking in terms of algorithms and basic Js knowledge before jumping into any frameworks. Coded in standard Javascript, HTML and CSS originally then ported to SASS.',
+    srcCodeLink: 'https://github.com/Nims93/Pathfinding-Visualiser',
+    liveLink: 'https://nims93.github.io/Pathfinding-Visualiser/',
   },
   {
     title: 'React Realtime Chat App',
     videoSrc: ReactRealtimeChatAppWebmDemo,
     altText: 'React Realtime Chat App webm',
-    desc: "A Realtime chat app made possible by Google Firebase Real-Time Database to store and serve dynamically created user messages. Also features user authentication using Google's authentication API for creating secure messages. The project that helped me fully grasp asyc Js for data fetching , writing and understanding collection based NoSQL Databases. Coded with React, SASS and Firebase Real-Time Database.",
+    desc: "A Realtime chat app made possible by Google Firebase Real-Time Database to store and read dynamically created user messages. Also features user authentication using Google's authentication API for creating secure messages. This project helped me fully grasp the concept of asyc Js for sending and fetching data accross a network and enhanced my understanding of collection based NoSQL Databases. I also dabbled in Node Js with the use of Firebase cloud functions to filter bad words from each message. Coded with React, SASS and Firebase Real-Time Database.",
     srcCodeLink: 'https://github.com/Nims93/react-realtime-chat-app',
     liveLink: 'https://realtime-react-chat-app.firebaseapp.com/',
   },
   {
-    title: 'Pathinding Visualiser',
-    videoSrc: PathfindingVisualiserWebmDemo,
-    altText: 'Pathfinding viualiser demo webm',
-    desc: 'An app that visualises pathfinding and maze generation algorithms. Create a maze of walls with mouse clicks, click draging your cursor across cells or using one of the maze generation algorithms. Then use a pathfinding algorithm to find a path (if one exists) from the start node to the end node. My first project that helped my sure up my basic Js knowledge before jumping into any frameworks. Coded in standard Javascript, HTML and SASS.',
-    srcCodeLink: 'https://github.com/Nims93/Pathfinding-Visualiser',
-    liveLink: 'https://nims93.github.io/Pathfinding-Visualiser/',
+    title: 'React Music Player',
+    videoSrc: ReactMusicAppWebmDemo,
+    altText: 'Music player app demo webm',
+    desc: 'A music player app that plays a selection of songs using the javascript Web Audio API. Features include repeat, next song, previous song, play/pause buttons and seekbar scrubbing to seek to a different part of the song. Able to handle variable playback speeds due to low download speed or use of the seekbar without the UI losing sync with the audio. Also features a nice elegant and responsive sliding UI design. Coded with React and SASS.',
+    srcCodeLink: 'https://github.com/Nims93/react-music-player',
+    liveLink:
+      'https://615504cc4d11864b80b8bc91--gallant-ride-7ea3b5.netlify.app/',
   },
 ];
 
 export default function Projects() {
   return (
-    <ProjectsSection id={'projects'}>
+    <ProjectsSection id="projects">
       <SVGContainer>
         <WaveSvg />
       </SVGContainer>
@@ -56,11 +57,47 @@ export default function Projects() {
           );
         })}
       </ProjectsWrapper>
+      <PortfolioInfoPara>
+        Finally, this portfolio website was coded using the Static Site
+        Generator Gatsby and styled using styled components. You can view the
+        source code here on
+        <Link href="https://github.com/Nims93/Portfolio-Website">
+          GitHub. <GitHubSvg />
+        </Link>
+        I originally coded this website with plain React and SCSS but decided
+        the scrap it and use a Static Site Generator instead. If you would like
+        to see Portfolio v1
+        <Link href="https://github.com/Nims93/Old-Portfolio-Site">
+          click here. <GitHubSvg />
+        </Link>
+      </PortfolioInfoPara>
     </ProjectsSection>
   );
 }
 
-// const
+const Link = styled.a`
+  color: white;
+  border-radius: 5px;
+  padding: 0 0.3em;
+
+  &:hover {
+    color: black;
+    background-color: white;
+    text-decoration: none;
+    transition: background-color 0.2s ease-in-out;
+    transition: color 0.4s ease-in-out;
+  }
+  svg {
+    height: 15px;
+    width: auto;
+  }
+`;
+
+const PortfolioInfoPara = styled.p`
+  font-size: 1.5em;
+  max-width: 1000px;
+  margin: auto;
+`;
 
 const Title = styled.h2`
   font-size: var(--section-heading-font-size-mb);
